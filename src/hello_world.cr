dc5bc067fcf28c5e1784a7e1298233f277c64a7d
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2018-05-27 22:53:12 +0200

    Update of src/hello_world.c

--- src/hello_world.cr
+++ src/hello_world.cr
@@ -1,6 +1,6 @@
 require "./hello_world/*"
 
-# HelloWorld is a nice bootstapping program to introduce
+# HelloWorld is a nice bootstrapping program to introduce
 # crystal language
 
 puts %|\033[1;31mHello, Crystal!\033[0m|
